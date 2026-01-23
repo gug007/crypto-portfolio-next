357324d054f379aefb63a7dc4fc324b2debe056f
gug007 <gurgen.abagyan@gmail.com>
2026-01-24 02:23:30 +0400

    add more words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           <div className="mx-auto flex max-w-4xl flex-col items-center text-center">
             <h1 className="inline-block bg-gradient-to-br from-[#0071e3] to-[#00c7be] bg-clip-text text-5xl font-semibold leading-tight tracking-tight text-transparent md:text-7xl md:leading-tight dark:from-[#2997ff] dark:to-[#64d2ff]">
               <TypewriterText 
-                words={["Free", "Best", "Fast"]} 
+                words={["Free", "Best", "Fast", "Secure", "Simple"]} 
                 className="inline-block bg-gradient-to-br from-[#0071e3] to-[#00c7be] bg-clip-text text-transparent dark:from-[#2997ff] dark:to-[#64d2ff]"
               /> <br />
               crypto portfolio <br />
